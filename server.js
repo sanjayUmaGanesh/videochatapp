fc00db4de0a103f31eddc0e5e016740eee65ad89
sanjayUmaGanesh <69663787+sanjayUmaGanesh@users.noreply.github.com>
2022-11-09 05:12:18 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,66 +1,145 @@
-const express = require("express");
-const app = express();
-const server = require("http").Server(app);
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+const socket = io("/");
 
-const { v4: uuidv4 } = require("uuid");
-
-const io = require("socket.io")(server, {
-    cors: {
-        origin: '*'
-    }
+var peer = new Peer(undefined, {
+    path: "/peerjs",
+    host: "/",
+    port: "443",
 });
 
-const { ExpressPeerServer } = require("peer");
-const peerServer = ExpressPeerServer(server, {
-    debug: true,
-});
+const user = prompt("Enter your name");
 
-app.use("/peerjs", peerServer);
+const myVideo = document.createElement("video");
+myVideo.muted = true;
 
-var nodemailer = require('nodemailer');
+let myStream;
 
-const transpoter = nodemailer.createTransport({
-    port:465,
-    host:"smtp.gmail.com",
-    auth:{user:'uma15ganesh@gmail.com',pass: 'jeqlptyowfyndbqw',},
-    secure:true,
+navigator.mediaDevices
+    .getUserMedia({
+        audio: true,
+        video: true,
+    })
+    .then((stream) => {
+        myStream = stream;
+        addVideoStream(myVideo, stream);
 
-});
-
-app.get("/", (req, res) => {
-    res.redirect(`/${uuidv4()}`);
-});
-
-app.get("/:room", (req, res) => {
-    res.render("index", { roomId: req.params.room });
-});
-
-app.post("/send-mail",(req,res)=>{
-    const to = req.body.to;
-    const url = req.body.url;
-    const mailData = {
-        from:"uma15ganesh@gmail.com",
-        to:to,
-        subject:"Join the video chat with me !!!",
-        html:`<p>Hey there, </p><p>Come and join me for a video chat here - ${url}</p>`
-    }
-    transpoter.sendMail(mailData,(error,info)=>{
-        if(error)
-        {return console.log(error)};
-        res.status(200).send({message:"invitation sent !",messgae_id: info.messageId});
-    });
-});
-
-io.on("connection", (socket) => {
-    socket.on("join-room", (roomId, userId, userName) => {
-        socket.join(roomId);
-        io.to(roomId).emit("user-connected", userId);
-        socket.on("message", (message) => {
-            io.to(roomId).emit("createMessage", message, userName);
+        socket.on("user-connected", (userId) => {
+            connectToNewUser(userId, stream);
         });
+
+        peer.on("call", (call) => {
+            call.answer(stream);
+            const video = document.createElement("video");
+            call.on("stream", (userVideoStream) => {
+                addVideoStream(video, userVideoStream);
+            });
+        });
+    })
+
+function connectToNewUser(userId, stream) {
+    const call = peer.call(userId, stream);
+    const video = document.createElement("video");
+    call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
     });
+};
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener("loadedmetadata", () => {
+        video.play();
+        $("#video_grid").append(video)
+    });
+};
+
+$(function () {
+    $("#show_chat").click(function () {
+        $(".left-window").css("display", "none")
+        $(".right-window").css("display", "block")
+        $(".header_back").css("display", "block")
+    })
+    $(".header_back").click(function () {
+        $(".left-window").css("display", "block")
+        $(".right-window").css("display", "none")
+        $(".header_back").css("display", "none")
+    })
+
+    $("#send").click(function () {
+        if ($("#chat_message").val().length !== 0) {
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
+        }
+    })
+
+    $("#chat_message").keydown(function (e) {
+        if (e.key == "Enter" && $("#chat_message").val().length !== 0) {
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
+        }
+    })
+
+    $("#mute_button").click(function () {
+        const enabled = myStream.getAudioTracks()[0].enabled;
+        if (enabled) {
+            myStream.getAudioTracks()[0].enabled = false;
+            html = `<i class="fas fa-microphone-slash"></i>`;
+            $("#mute_button").toggleClass("background_red");
+            $("#mute_button").html(html)
+        } else {
+            myStream.getAudioTracks()[0].enabled = true;
+            html = `<i class="fas fa-microphone"></i>`;
+            $("#mute_button").toggleClass("background_red");
+            $("#mute_button").html(html)
+        }
+    })
+
+    $("#stop_video").click(function () {
+        const enabled = myStream.getVideoTracks()[0].enabled;
+        if (enabled) {
+            myStream.getVideoTracks()[0].enabled = false;
+            html = `<i class="fas fa-video-slash"></i>`;
+            $("#stop_video").toggleClass("background_red");
+            $("#stop_video").html(html)
+        } else {
+            myStream.getVideoTracks()[0].enabled = true;
+            html = `<i class="fas fa-video"></i>`;
+            $("#stop_video").toggleClass("background_red");
+            $("#stop_video").html(html)
+        }
+    })
+
+    $("#invite_button").click(function () {
+        const to = prompt("Enter the email address")
+        let data = {
+            url: window.location.href,
+            to: to
+        }
+        $.ajax({
+            url: "/send-mail",
+            type: "post",
+            data: JSON.stringify(data),
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (result) {
+                alert("Invite sent!")
+            },
+            error: function (result) {
+                console.log(result.responseJSON)
+            }
+        })
+    })
+
+})
+
+peer.on("open", (id) => {
+    socket.emit("join-room", ROOM_ID, id, user);
 });
 
-server.listen(process.env.PORT || 3030);
+socket.on("createMessage", (message, userName) => {
+    $(".messages").append(`
+        <div class="message">
+            <b><i class="far fa-user-circle"></i> <span> ${userName === user ? "me" : userName
+        }</span> </b>
+            <span>${message}</span>
+        </div>
+    `)
+});
